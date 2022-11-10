3efaa734ff60a489cf4bbe9a209a26a1e704b73d
Roucky <geoffrey.chauwin@etudiants.univ-reims.fr>
2022-11-10 07:53:03 +0100

    creation of ingredients, ustensils and appliances list

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,40 @@
+const Ingredients = [];
+const Appliances = [];
+const Tools = [];
+
 async function getRecipes() {
     const response = await fetch("data/recipes.json");
     const data = await response.json();
 
     return data;
+}
+
+// ingredients, appliance, ustensils
+async function getFilters(data){
+
+    data.forEach((recipe) =>{
+
+        // TODO remove plurial from ingredients const
+
+        recipe.ingredients.forEach((ingredient)=>{
+            if(!Ingredients.includes(ingredient.ingredient.toLowerCase()) && !Ingredients.includes(ingredient.ingredient.toLowerCase().substring(0, ingredient.ingredient.length - 1))){
+                Ingredients.push(ingredient.ingredient.toLowerCase());
+            }
+        })
+
+        if(!Appliances.includes(recipe.appliance.toLowerCase())){
+            Appliances.push(recipe.appliance.toLowerCase());
+        }
+
+        recipe.ustensils.forEach((tool)=>{
+            if(!Tools.includes(tool.toLowerCase())){
+                Tools.push(tool.toLowerCase());
+            }
+        })
+    });
+    console.log("Ingredients",Ingredients.sort())
+    console.log("Appliances",Appliances)
+    console.log("Tools",Tools)
 }
 
 // display de data of each recipes
@@ -19,6 +51,7 @@ async function displayRecipes(recipes) {
 // start all functions allowing the display of the page's element
 async function init() {
     const { recipes } = await getRecipes();
+    getFilters(recipes);
     displayRecipes(recipes);
 }
 
